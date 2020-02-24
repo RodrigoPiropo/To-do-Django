7d046b4380017eec0f3ee40baac7862de1a96a9d
RodrigoAP1307 <rodrigoap1307@gmail.com>
2020-02-23 23:29:40 -0300

    Added form linked to the database to place the task

---
+++ todo/task/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import *
+
+
+class TaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = '__all__'
--- todo/task/views.py
+++ todo/task/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from .models import Task
 
-# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'task/list.html', context)
