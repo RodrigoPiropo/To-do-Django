d467e5f40db1d790917b2aa3ab0ceea558f3761e
RodrigoAP1307 <rodrigoap1307@gmail.com>
2020-02-24 02:10:59 -0300

    Created delete functionality

--- todo/task/urls.py
+++ todo/task/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='list'),
     path('update_task/<str:pk>/', views.updateTask, name='update_task'),
+    path('delete_task/<str:pk>/', views.deleteTask, name='delete_task'),
 
 ]
--- todo/task/views.py
+++ todo/task/views.py
@@ -30,3 +30,14 @@ def updateTask(request, pk):
         return redirect('/')
     context = {'form': form}
     return render(request, 'task/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'task/delete.html', context)
