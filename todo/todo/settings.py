e74f1d53c7f2c5bb31bdf49f55b96c37260ea567
RodrigoAP1307 <rodrigoap1307@gmail.com>
2020-02-24 16:19:53 -0300

    Translating and stylizing two templates with the bootstrap

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     # My apps:
     'task',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'task/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
